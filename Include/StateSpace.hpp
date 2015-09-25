40149085e15351019e0d878d88ac7662f0c558d9
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2015-09-24 23:01:12 -0400

    Ultimately move code into StateSpace class + derived classes?

---
+++ Include/StateSpace.hpp
@@ -0,0 +1,10 @@
+#ifndef STATESPACE_HPP
+#define STATESPACE_HPP
+
+#include <mkl.h>
+#include <mkl_types.h>
+
+using namespace std;
+
+class StateSpaceModel {
+public:
---
+++ src/StateSpace.cpp
@@ -0,0 +1,17 @@
+#include <malloc.h>
+#include <sys/time.h>
+#include <limits>
+#include <mathimf.h>
+#include <omp.h>
+#include <mkl.h>
+#include <mkl_types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include "Constants.hpp"
+#include "StateSpace.hpp"
+
+using namespace std;
+
+StateSpace::StateSpaceModel
