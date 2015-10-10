bb8476ab111fefedf5708e631aecdba85066a3ba
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2015-10-10 07:36:34 -0400

    Pondering about how things work....

---
+++ testQInt.py
@@ -0,0 +1,45 @@
+import numpy as np
+import numpy.linalg as la
+import scipy.linalg as sla
+import scipy.integrate as sint
+import math as m
+
+
+def Qint(dchi,A,B,i,j):
+	#ans = ((sla.expm(A*dchi))*B*(B.T)*(sla.expm((A.T)*dchi)))[i,j]
+	ans = ((sla.expm(A*dchi))*B*(B.T)*((sla.expm(A*dchi)).T))[i,j]
+	return ans
+
+#dt = 0.02
+dt = 0.020489543562895264
+A = np.matrix(np.zeros((2,2)))
+B = np.matrix(np.zeros((2,1)))
+D = np.matrix(np.zeros((2,1)))
+
+A[0,0] = -0.75
+A[1,0] = -0.01
+A[0,1] = 1.0
+
+B[0,0] = 1.2*m.pow(10.0,-9.0)
+B[1,0] = 7.0*m.pow(10.0,-9.0)
+
+for i in xrange(2):
+	D[i,0]=m.sqrt(sint.quad(Qint,0,dt,args=(A,B,i,i),epsrel=0.0001)[0]) # Tested
+	'''D[i,0]=sint.quad(Qint,0,dt,args=(A,B,i,i),epsrel=0.0001)[0]''' # Untested
+Q = D*(D.T)
+
+print "A"
+print A
+print
+
+print "B"
+print B
+print
+
+print "D"
+print D
+print
+
+print "Q"
+print Q
+print
