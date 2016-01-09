b54856c8223404a11bd18f9e7611a99bfe59560f
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2016-01-09 10:41:35 -0500

    Makes DemoScript.py run-able in reasonable time

--- python/DemoScript.py
+++ python/DemoScript.py
@@ -2,6 +2,7 @@
 
 import argparse as argparse
 import sys
+import os
 import time
 import pdb
 
@@ -17,25 +18,6 @@ parser.add_argument("-o", "--old", help = "DateTime of run to be used")
 parser.add_argument("-v", "--verbose", help = "Verbose T/F")
 args = parser.parse_args()
 
-try:
-	TestFile = open(args.pwd + 'Irregular_MMDDYYYYHHMMSS.log', 'r')
-	TestFile.close()
-except IOError:
-	plotSuppliedLCTask(args.pwd, 'Irregular.ini', 'MMDDYYYYHHMMSS').run()
-fitCARMATask(args.pwd, 'Irregular.ini', 'MMDDYYYYHHMMSS').run()
-try:
-	TestFile = open(args.pwd + 'Regular_MMDDYYYYHHMMSS.log', 'r')
-	TestFile.close()
-except IOError:
-	plotSuppliedLCTask(args.pwd, 'Regular.ini', 'MMDDYYYYHHMMSS').run()
-fitCARMATask(args.pwd, 'Regular.ini', 'MMDDYYYYHHMMSS').run()
-try:
-	TestFile = open(args.pwd + 'Missing_MMDDYYYYHHMMSS.log', 'r')
-	TestFile.close()
-except IOError:
-	plotSuppliedLCTask(args.pwd, 'Missing.ini', 'MMDDYYYYHHMMSS').run()
-fitCARMATask(args.pwd, 'Missing.ini', 'MMDDYYYYHHMMSS').run()
-
 if args.old:
 	try:
 		TestFile = open(args.pwd + args.cf.split('.')[0] + '_' + args.old + '.lc', 'r')
@@ -43,9 +25,47 @@ if args.old:
 	except:
 		print args.pwd + args.cf.split('.')[0] + '_' + args.old + '_LC.dat not found!'
 		sys.exit(1)
+	Stamp = args.old
 	plotPSDTask(args.pwd, args.cf, args.old).run()
 	makeMockLCTask(args.pwd, args.cf, args.old).run()
 else:
 	TimeStr = time.strftime("%m%d%Y") + time.strftime("%H%M%S")
+	Stamp = TimeStr
 	plotPSDTask(args.pwd, args.cf, TimeStr).run()
 	makeMockLCTask(args.pwd, args.cf, TimeStr).run()
+
+cmd = 'cp %s%s %sRegular.lc'%(args.pwd, args.cf.split('.')[0] + '_' + Stamp + '.lc', args.pwd)
+os.system(cmd)
+
+cmd = 'python python/writeMasked+Irregular.py'
+os.system(cmd)
+
+try:
+	TestFile = open(args.pwd + 'Regular_MMDDYYYYHHMMSS.log', 'r')
+	TestFile.close()
+except IOError:
+	plotSuppliedLCTask(args.pwd, 'Regular.ini', 'MMDDYYYYHHMMSS').run()
+try:
+	TestFile = open(args.pwd + 'Regular_MMDDYYYYHHMMSS_CARMAResult.dat', 'r')
+except IOError:
+	fitCARMATask(args.pwd, 'Regular.ini', 'MMDDYYYYHHMMSS').run()
+
+try:
+	TestFile = open(args.pwd + 'Irregular_MMDDYYYYHHMMSS.log', 'r')
+	TestFile.close()
+except IOError:
+	plotSuppliedLCTask(args.pwd, 'Irregular.ini', 'MMDDYYYYHHMMSS').run()
+try:
+	TestFile = open(args.pwd + 'Irregular_MMDDYYYYHHMMSS_CARMAResult.dat', 'r')
+except IOError:
+	fitCARMATask(args.pwd, 'Irregular.ini', 'MMDDYYYYHHMMSS').run()
+
+try:
+	TestFile = open(args.pwd + 'Missing_MMDDYYYYHHMMSS.log', 'r')
+	TestFile.close()
+except IOError:
+	plotSuppliedLCTask(args.pwd, 'Missing.ini', 'MMDDYYYYHHMMSS').run()
+try:
+	TestFile = open(args.pwd + 'Missing_MMDDYYYYHHMMSS_CARMAResult.dat', 'r')
+except IOError:
+	fitCARMATask(args.pwd, 'Missing.ini', 'MMDDYYYYHHMMSS').run()
