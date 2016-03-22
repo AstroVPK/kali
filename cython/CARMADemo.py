79973b8b8e8d41bae8ecad3f89cae348b88d674c
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2016-03-22 02:07:03 -0400

    Make CARMADemo.py run with CARMA(3,1) model
    CARMADemo.py now runs with a CARMA(3,1) model.

--- cython/CARMADemo.py
+++ cython/CARMADemo.py
@@ -7,9 +7,9 @@ import lc
 import rand
 import CARMATask
 
-numCadences = 1000
+T = 1000
 dt = 0.1
-T = numCadences*dt
+numCadences = int(T/dt)
 IR = False
 tolIR = 1.0e-3
 fracIntrinsicVar = 0.15
@@ -19,15 +19,22 @@ minTimescale = 5.0e-1
 maxTimescale = 5.0
 
 r = lc.basicLC(numCadences, dt = dt, IR = IR, tolIR = tolIR, fracIntrinsicVar = fracIntrinsicVar, fracSignalToNoise = fracSignalToNoise, maxSigma = maxSigma, minTimescale = minTimescale, maxTimescale = maxTimescale)
-newTask = CARMATask.CARMATask(1,0)
 
-Theta = np.zeros(2)
-Theta[0] = 0.1
-Theta[1] = 1.0
+p = 3
+q = 1
+
+Theta = np.zeros(p + q + 1)
+Theta[0] = 1.27
+Theta[1] = 0.402
+Theta[2] = 5.23e-3
+Theta[3] = 7.0e-9
+Theta[4] = 1.2e-9
+
+newTask = CARMATask.CARMATask(p,q)
 
 res = newTask.printSystem(dt, Theta)
 
-Sigma = np.zeros(1)
+Sigma = np.zeros(p*p)
 
 res = newTask.getSigma(dt, Theta, Sigma)
 
@@ -60,5 +67,3 @@ plt.plot(r.t, r.x + meanFlux, color = '#7570b3', zorder = 5, label = r'Intrinsic
 plt.errorbar(r.t, r.y + meanFlux, r.yerr, fmt = '.', capsize = 0, color = '#d95f02', markeredgecolor = 'none', zorder = 10, label = r'Observed LC: $\ln \mathcal{P} = %+e$'%(LnPosterior))
 plt.legend()
 plt.show()
-
-pdb.set_trace()
