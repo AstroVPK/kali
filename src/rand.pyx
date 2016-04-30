4790a4064ffd2ceff94ace3b73644ae0ba53bd74
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2016-04-29 20:00:12 -0400

    Changed RuntimeError to Warning

--- src/rand.pyx
+++ src/rand.pyx
@@ -2,6 +2,7 @@
 
 import numpy as np
 cimport numpy as np
+import warnings
 import cython
 DTYPE = np.uint32
 ctypedef np.uint32_t DTYPE_t
@@ -15,6 +16,6 @@ def rdrand(np.ndarray[unsigned int, ndim=1, mode="c"] inputArr not None):
 	cdef int numRand = inputArr.shape[0]
 	success = rdrand_get_n_32(numRand, &inputArr[0])
 	if success != 1:
-		raise RuntimeError('Intel RDRAND failed with error code %d! Using numpy.random'%(success))
+		warnings.warn('Intel RDRAND failed with error code %d! Using numpy.random'%(success))
 		inputArr = np.random.randint(0, 4294967295, numRand)
 	return success
