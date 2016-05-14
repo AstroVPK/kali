21f32661c318a82642303d1ea18fd4fdf580f176
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2016-05-14 13:53:10 -0400

    Clean up bin/setup.sh script.
    We have cleaned up the bin/setup.sh script so that it only exports
    absolutely necessary enviroment variables to allow libcarma to run.
    the ccfi branch is no longer setup automatically and users wishing
    to use it must now explicitly point PYTHONPATH at the cffi
    directory.

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,16 +3,9 @@
 # source this file from your ~/.bashrc
 #
 # relative to <libcarma>/bin/
-LIBCARMA=$(cd "$(dirname "$BASH_SOURCE")/.."; pwd)
-LIBCARMA_CFFI=$(cd "$(dirname "$BASH_SOURCE")/../cffi"; pwd)
 LIBCARMA_CYTHON=$(cd "$(dirname "$BASH_SOURCE")/../python"; pwd)
 LIBCARMA_CYTHON_LIB=$(cd "$(dirname "$BASH_SOURCE")/../lib"; pwd)
 
-export LIBCARMA="$LIBCARMA"
-export LIBCARMA_CFFI="$LIBCARMA_CFFI"
-export LIBCARMA_CYTHON="$LIBCARMA_CYTHON"
-export LIBCARMA_CYTHON_LIB="$LIBCARMA_CYTHON_LIB"
-export PYTHONPATH="$LIBCARMA_CFFI:$PYTHONPATH"
 export PYTHONPATH="$LIBCARMA_CYTHON:$PYTHONPATH"
 export PYTHONPATH="$LIBCARMA_CYTHON_LIB:$PYTHONPATH"
 export OMP_NUM_THREADS=4
