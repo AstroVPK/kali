afa254d767876c25ca8df146cdffb896518cf425
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2016-05-15 12:21:45 -0400

    Labels are now passed from outside to vizTriangle

--- python/util/mcmcviz.py
+++ python/util/mcmcviz.py
@@ -111,16 +111,11 @@ def vizWalkers(Chain, LogPosterior, dim1, dim1Name, dim2, dim2Name):
 	plt.show()
 	return 0
 
-def vizTriangle(p, q, Chain, figTitle):
+def vizTriangle(p, q, Chain, labelList, figTitle):
 	ndims = Chain.shape[0]
 	nwalkers = Chain.shape[1]
 	nsteps = Chain.shape[2]
-	lbls = list()
-	for i in range(p):
-		lbls.append("$a_{%d}$"%(i + 1))
-	for i in range(q + 1):
-		lbls.append("$b_{%d}$"%(i))
 
 	flatChain = np.swapaxes(copy.copy(Chain[:,:,nsteps/2:]).reshape((ndims, -1), order = 'F'), axis1 = 0, axis2 = 1)
-	fig0, quantiles, qvalues = triangle.corner(flatChain, labels = lbls, fig_size = 10.0, show_titles = True, fig_title = figTitle, title_args = {'fontsize': 12}, quantiles = [0.16, 0.5, 0.84], verbose = False, plot_contours = True, plot_datapoints = True, plot_contour_lines = False, pcolor_cmap = cm.gist_earth)
+	fig0, quantiles, qvalues = triangle.corner(flatChain, labels = labelList, fig_size = 10.0, show_titles = True, fig_title = figTitle, title_args = {'fontsize': 12}, quantiles = [0.16, 0.5, 0.84], verbose = False, plot_contours = True, plot_datapoints = True, plot_contour_lines = False, pcolor_cmap = cm.gist_earth)
 	plt.show()
