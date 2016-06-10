Fixes SF computation
The estimated SF was not being computed correctly for irregular
light curves because the light curve itself was not being passed
correctly. This commit fixes that problem.

examples/k2Fit.py
@@ -48,8 +48,8 @@ parser.add_argument('-nsteps', '--nsteps', type = int, default = 250, help = r'N
 parser.add_argument('-nwalkers', '--nwalkers', type = int, default = 25*psutil.cpu_count(logical = True), help = r'Number of walkers')
 parser.add_argument('-pMax', '--pMax', type = int, default = 1, help = r'Maximum C-AR order')
 parser.add_argument('-pMin', '--pMin', type = int, default = 1, help = r'Minimum C-AR order')
-parser.add_argument('-qMax', '--qMax', type = int, default = 0, help = r'Maximum C-MA order')
-parser.add_argument('-qMin', '--qMin', type = int, default = 0, help = r'Minimum C-MA order')
+parser.add_argument('-qMax', '--qMax', type = int, default = -1, help = r'Maximum C-MA order')
+parser.add_argument('-qMin', '--qMin', type = int, default = -1, help = r'Minimum C-MA order')
 parser.add_argument('--plot', dest = 'plot', action = 'store_true', help = r'Show plot?')
 parser.add_argument('--no-plot', dest = 'plot', action = 'store_false', help = r'Do not show plot?')
 parser.set_defaults(plot = True)
@@ -65,6 +65,9 @@ parser.set_defaults(save = False)
 parser.add_argument('--log10', dest = 'log10', action = 'store_true', help = r'Compute distances in log space?')
 parser.add_argument('--no-log10', dest = 'log10', action = 'store_false', help = r'Do not compute distances in log space?')
 parser.set_defaults(log10 = False)
+parser.add_argument('--viewer', dest = 'viewer', action = 'store_true', help = r'Visualize MCMC walkers')
+parser.add_argument('--no-viewer', dest = 'viewer', action = 'store_false', help = r'Do not visualize MCMC walkers')
+parser.set_defaults(viewer = True)
 args = parser.parse_args()
 
 if (args.qMax >= args.pMax):
@@ -111,37 +114,82 @@ print 'Best model is C-ARMA(%d,%d)'%(pBest, qBest)
 
 bestTask = taskDict['%d %d'%(pBest, qBest)]
 
-var = str(raw_input('Do you wish to view any MCMC walkers? (y/n):')).lower()
-if var == 'y':
+if args.viewer:
 	notDone = True
 	while notDone:
+		whatToView = -1
+		while whatToView < 0 or whatToView > 3:
+			whatToView = int(raw_input('View walkers in C-ARMA coefficients (0) or C-ARMA roots (1) or C-ARMA timescales (2):'))
 		pView = -1
 		while pView < 1 or pView > args.pMax:
 			pView = int(raw_input('C-AR model order:'))
 		qView = -1
 		while qView < 0 or qView >= pView:
 			qView = int(raw_input('C-MA model order:'))
+
 		dim1 = -1
-		while dim1 < 0 or dim1 > pView + qView + 1:
+		while dim1 < 0 or dim1 > pView + qView:
 			dim1 = int(raw_input('1st Dimension to view:'))
 		dim2 = -1
-		while dim2 < 0 or dim2 > pView + qView + 1 or dim2 == dim1:
+		while dim2 < 0 or dim2 > pView + qView or dim2 == dim1:
 			dim2 = int(raw_input('2nd Dimension to view:'))
 
-		if dim1 < pView:
-			dim1Name = r'$a_{%d}$'%(dim1)
-		if dim1 >= pView and dim1 < pView + qView + 1:
-			dim1Name = r'$b_{%d}$'%(dim1 - pView)
+		if whatToView == 0:
+			if dim1 < pView:
+				dim1Name = r'$a_{%d}$'%(dim1)
+			if dim1 >= pView and dim1 < pView + qView + 1:
+				dim1Name = r'$b_{%d}$'%(dim1 - pView)
+			if dim2 < pView:
+				dim2Name = r'$a_{%d}$'%(dim2)
+			if dim2 >= pView and dim2 < pView + qView + 1:
+				dim2Name = r'$b_{%d}$'%(dim2 - pView)
+			res = mcmcviz.vizWalkers(taskDict['%d %d'%(pView, qView)].Chain, taskDict['%d %d'%(pView, qView)].LnPosterior, dim1, dim1Name, dim2, dim2Name)
 
-		if dim2 < pView:
-			dim2Name = r'$a_{%d}$'%(dim2)
-		if dim2 >= pView and dim2 < pView + qView + 1:
-			dim2Name = r'$b_{%d}$'%(dim2 - pView)
+		elif whatToView == 1:
+			if dim1 < pView:
+				dim1Name = r'$r_{%d}$'%(dim1)
+			if dim1 >= pView and dim1 < pView + qView:
+				dim1Name = r'$m_{%d}$'%(dim1 - pView)
+			if dim1 == pView + qView:
+				dim1Name = r'$\mathrm{Amp.}$'
+			if dim2 < pView:
+				dim2Name = r'$r_{%d}$'%(dim2)
+			if dim2 >= pView and dim2 < pView + qView:
+				dim2Name = r'$m_{%d}$'%(dim2 - pView)
+			if dim2 == pView + qView:
+				dim2Name = r'$\mathrm{Amp.}$'
+			res = mcmcviz.vizWalkers(taskDict['%d %d'%(pView, qView)].rootChain, taskDict['%d %d'%(pView, qView)].LnPosterior, dim1, dim1Name, dim2, dim2Name)
 
-		res = mcmcviz.vizWalkers(taskDict['%d %d'%(pView, qView)].Chain, taskDict['%d %d'%(pView, qView)].LnPosterior, dim1, dim1Name, dim2, dim2Name)
+		else:
+			if dim1 < pView + qView:
+				dim1Name = r'$\tau_{%d}$'%(dim1)
+			if dim1 == pView + qView:
+				dim1Name = r'$\mathrm{Amp.}$'
+			if dim2 < pView + qView:
+				dim2Name = r'$\tau_{%d}$'%(dim2)
+			if dim2 == pView + qView:
+				dim2Name = r'$\mathrm{Amp.}$'
+			res = mcmcviz.vizWalkers(taskDict['%d %d'%(pView, qView)].timescaleChain, taskDict['%d %d'%(pView, qView)].LnPosterior, dim1, dim1Name, dim2, dim2Name)
+
 		var = str(raw_input('Do you wish to view any more MCMC walkers? (y/n):')).lower()
 		if var == 'n':
 			notDone = False
+
+Theta = bestTask.Chain[:,np.where(bestTask.LnPosterior == np.max(bestTask.LnPosterior))[0][0],np.where(bestTask.LnPosterior == np.max(bestTask.LnPosterior))[1][0]]
+nt = libcarma.basicTask(pBest, qBest)
+nt.set(LC.dt, Theta)
+nt.smooth(LC)
+LC.plot()
+
+plt.figure(1, figsize = (fwid, fhgt))
+lagsEst, sfEst, sferrEst = LC.sf()
+lagsModel, sfModel = bestTask.sf(start = lagsEst[1], stop = lagsEst[-1], num = 5000, spacing = 'log')
+plt.loglog(lagsModel, sfModel, label = r'$SF(\delta t)$ (model)', color = '#000000', zorder = 5)
+plt.errorbar(lagsEst, sfEst, sferrEst, label = r'$SF(\delta t)$ (est)', fmt = 'o', capsize = 0, color = '#ff7f00', markeredgecolor = 'none', zorder = 0)
+plt.xlabel(r'$\log_{10}\delta t$')
+plt.ylabel(r'$\log_{10} SF$')
+plt.legend(loc = 2)
+plt.show()
 
 if args.stop:
 	pdb.set_trace()

examples/sdssFit.py
@@ -186,15 +186,25 @@ if args.viewer:
 				dim2Name = r'$\mathrm{Amp.}$'
 			res = mcmcviz.vizWalkers(taskDict['%d %d'%(pView, qView)].timescaleChain, taskDict['%d %d'%(pView, qView)].LnPosterior, dim1, dim1Name, dim2, dim2Name)
 
-		Theta = bestTask.Chain[:,np.where(bestTask.LnPosterior == np.max(bestTask.LnPosterior))[0][0],np.where(bestTask.LnPosterior == np.max(bestTask.LnPosterior))[1][0]]
-		nt = libcarma.basicTask(pBest, qBest)
-		nt.set(sdssLC.dt, Theta)
-		nt.smooth(sdssLC)
-		sdssLC.plot()
-
 		var = str(raw_input('Do you wish to view any more MCMC walkers? (y/n):')).lower()
 		if var == 'n':
 			notDone = False
+
+Theta = bestTask.Chain[:,np.where(bestTask.LnPosterior == np.max(bestTask.LnPosterior))[0][0],np.where(bestTask.LnPosterior == np.max(bestTask.LnPosterior))[1][0]]
+nt = libcarma.basicTask(pBest, qBest)
+nt.set(sdssLC.dt, Theta)
+nt.smooth(sdssLC)
+sdssLC.plot()
+
+plt.figure(1, figsize = (fwid, fhgt))
+lagsEst, sfEst, sferrEst = sdssLC.sf()
+lagsModel, sfModel = bestTask.sf(start = lagsEst[1], stop = lagsEst[-1], num = 5000, spacing = 'log')
+plt.loglog(lagsModel, sfModel, label = r'$SF(\delta t)$ (model)', color = '#000000', zorder = 5)
+plt.errorbar(lagsEst, sfEst, sferrEst, label = r'$SF(\delta t)$ (est)', fmt = 'o', capsize = 0, color = '#ff7f00', markeredgecolor = 'none', zorder = 0)
+plt.xlabel(r'$\log_{10}\delta t$')
+plt.ylabel(r'$\log_{10} SF$')
+plt.legend(loc = 2)
+plt.show()
 
 if args.stop:
 	pdb.set_trace()

python/libcarma.py
@@ -238,14 +238,20 @@ class lc(object):
 			self.y = np.require(np.zeros(self.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of observed fluxes.
 			self.yerr = np.require(np.zeros(self.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of observed flux errors.
 			self.mask = np.require(np.zeros(self.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of mask values.
-			self.XSim = np.require(np.zeros(self._p), requirements=['F', 'A', 'W', 'O', 'E']) ## State of light curve at last timestamp
-			self.PSim = np.require(np.zeros(self._p*self._p), requirements=['F', 'A', 'W', 'O', 'E']) ## Uncertainty in state of light curve at last timestamp.
-			self.XComp = np.require(np.zeros(self._p), requirements=['F', 'A', 'W', 'O', 'E']) ## State of light curve at last timestamp
-			self.PComp = np.require(np.zeros(self._p*self._p), requirements=['F', 'A', 'W', 'O', 'E']) ## Uncertainty in state of light curve at last timestamp.
+			self.XSim = np.require(np.zeros(self.pSim), requirements=['F', 'A', 'W', 'O', 'E']) ## State of light curve at last timestamp
+			self.PSim = np.require(np.zeros(self.pSim*self.pSim), requirements=['F', 'A', 'W', 'O', 'E']) ## Uncertainty in state of light curve at last timestamp.
+			self.XComp = np.require(np.zeros(self.pComp), requirements=['F', 'A', 'W', 'O', 'E']) ## State of light curve at last timestamp
+			self.PComp = np.require(np.zeros(self.pComp*self.pComp), requirements=['F', 'A', 'W', 'O', 'E']) ## Uncertainty in state of light curve at last timestamp.
 			self._name = str(name) ## The name of the light curve (usually the object's name).
 			self._band = str(band) ## The name of the photometric band (eg. HSC-I or SDSS-g etc..).
-			self._xunit = r'$' + str(xunit) + '$' ## Unit in which time is measured (eg. s, sec, seconds etc...).
-			self._yunit = r'$' + str(yunit) + '$' ## Unit in which the flux is measured (eg Wm^{-2} etc...).
+			if str(xunit)[0] != '$':
+				self._xunit = r'$' + str(xunit) + '$' ## Unit in which time is measured (eg. s, sec, seconds etc...).
+			else:
+				self._xunit = str(xunit)
+			if str(yunit)[0] != '$':
+				self._yunit = r'$' + str(yunit) + '$' ## Unit in which the flux is measured (eg Wm^{-2} etc...).
+			else:
+				self._yunit = str(yunit)
 			self._tolIR = tolIR ## Tolerance on the irregularity. If IR == False, this parameter is not used. Otherwise, a timestep is irregular iff abs((t_incr - dt)/((t_incr + dt)/2.0)) > tolIR where t_incr is the new increment in time and dt is the previous increment in time.
 			self._fracIntrinsicVar = fracIntrinsicVar
 			self._fracNoiseToSignal = fracNoiseToSignal
@@ -271,6 +277,11 @@ class lc(object):
 	@property
 	def numCadences(self):
 		return self._numCadences
+
+	@numCadences.setter
+	def numCadences(self, value):
+		self._lcCython.numCadences = value
+		self._numCadences = value
 
 	@property
 	def simulatedCadenceNum(self):
@@ -398,7 +409,10 @@ class lc(object):
 
 	@xunit.setter
 	def xunit(self, value):
-		self._xunit = str(value)
+		if str(value)[0] != r'$':
+			self._xunit = r'$' + str(value) + r'$' 
+		else:
+			self._xunit = str(value)
 
 	@property
 	def yunit(self):
@@ -406,7 +420,10 @@ class lc(object):
 
 	@yunit.setter
 	def yunit(self, value):
-		self._yunit = str(value)
+		if str(value)[0] != r'$':
+			self._yunit = r'$' + str(value) + r'$' 
+		else:
+			self._yunit = str(value)
 
 	@property
 	def tolIR(self):
@@ -841,7 +858,7 @@ class lc(object):
 				raise ValueError('newdt cannot be greater than dt')
 			newLC = self.copy()
 			newLC.dt = newdt
-			newLC._numCadences = int(self.dt/newLC.dt)*self.numCadences
+			newLC.numCadences = int(math.ceil(self.T/newLC.dt))
 			del newLC.t
 			del newLC.x
 			del newLC.y
@@ -861,21 +878,23 @@ class lc(object):
 				newLC.y[index] = self.y[i]
 				newLC.yerr[index] = self.yerr[i]
 				newLC.mask[index] = 1.0
-			count = int(np.sum(self.mask[i]))
+			count = int(np.sum(newLC.mask[i]))
 			y_meanSum = 0.0
 			yerr_meanSum = 0.0
-			for i in xrange(self.numCadences):
-				y_meanSum += self.mask[i]*self.y[i]
-				yerr_meanSum += self.mask[i]*self.yerr[i]
-			self._mean = y_meanSum/count
-			self._meanerr = yerr_meanSum/count
+			for i in xrange(newLC.numCadences):
+				y_meanSum += newLC.mask[i]*newLC.y[i]
+				yerr_meanSum += newLC.mask[i]*newLC.yerr[i]
+			if count > 0:
+				newLC._mean = y_meanSum/count
+				newLC._meanerr = yerr_meanSum/count
 			y_stdSum = 0.0
 			yerr_stdSum = 0.0
-			for i in xrange(self.numCadences):
-				y_stdSum += math.pow(self.mask[i]*self.y[i] - self._mean, 2.0)
-				yerr_stdSum += math.pow(self.mask[i]*self.yerr[i] - self._meanerr, 2.0)
-			self._std = math.sqrt(y_stdSum/count)
-			self._stderr = math.sqrt(yerr_stdSum/count)
+			for i in xrange(newLC.numCadences):
+				y_stdSum += math.pow(newLC.mask[i]*newLC.y[i] - newLC._mean, 2.0)
+				yerr_stdSum += math.pow(newLC.mask[i]*newLC.yerr[i] - newLC._meanerr, 2.0)
+			if count > 0:
+				newLC._std = math.sqrt(y_stdSum/count)
+				newLC._stderr = math.sqrt(yerr_stdSum/count)
 
 			return newLC
 		else:
@@ -892,7 +911,7 @@ class lc(object):
 		lags = np.require(np.zeros(useLC.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of timestamps.
 		acvf = np.require(np.zeros(useLC.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of intrinsic fluxes.
 		acvferr = np.require(np.zeros(useLC.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of intrinsic fluxes.
-		useLC._lcCython.compute_ACVF(lags, acvf, acvferr)
+		useLC._lcCython.compute_ACVF(useLC.t, useLC.x, useLC.y, useLC.yerr, useLC.mask, lags, acvf, acvferr)
 		return lags, acvf, acvferr
 
 	def acf(self, newdt = None):
@@ -905,7 +924,7 @@ class lc(object):
 		acvferr = np.require(np.zeros(useLC.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of intrinsic fluxes.
 		acf = np.require(np.zeros(useLC.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of intrinsic fluxes.
 		acferr = np.require(np.zeros(useLC.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of intrinsic fluxes.
-		res = useLC._lcCython.compute_ACVF(lags, acvf, acvferr)
+		res = useLC._lcCython.compute_ACVF(useLC.t, useLC.x, useLC.y, useLC.yerr, useLC.mask, lags, acvf, acvferr)
 		acf = acvf/acvf[0]
 		acferr = np.sqrt(np.power(acvf/acvf[0], 2.0)*(np.power(acvferr/acvf, 2.0) + math.pow(acvferr[0]/acvf[0], 2.0)))
 		return res
@@ -918,10 +937,10 @@ class lc(object):
 		lags = np.require(np.zeros(useLC.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of timestamps.
 		sf = np.require(np.zeros(useLC.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of intrinsic fluxes.
 		sferr = np.require(np.zeros(useLC.numCadences), requirements=['F', 'A', 'W', 'O', 'E']) ## Numpy array of intrinsic fluxes.
-		useLC._lcCython.compute_SF(lags, sf, sferr)
+		useLC._lcCython.compute_SF(useLC.t, useLC.x, useLC.y, useLC.yerr, useLC.mask, lags, sf, sferr)
 		return lags, sf, sferr
 
-	def plot(self):
+	def plot(self, doShow = False):
 		plt.figure(-1, figsize = (fwid, fhgt))
 		if np.sum(self.x) != 0.0:
 			plt.plot(self.t, self.x - np.mean(self.x) + np.mean(self.y[np.where(self.mask == 1.0)[0]]), color = '#984ea3', zorder = 0)
@@ -935,8 +954,8 @@ class lc(object):
 		plt.ylabel(self.yunit)
 		plt.title(r'Light curve')
 		plt.legend()
-		plt.show(False)
-		plt.show()
+		if doShow:
+			plt.show(False)
 
 class lcIterator(object):
 	def __init__(self, t, x, y, yerr, mask):
@@ -966,18 +985,16 @@ class lcIterator(object):
 class basicLC(lc):
 
 	def copy(self):
-		lccopy = basicLC(self._numCadences, dt = self._dt, dtSmooth = self._dtSmooth, name = None, band = self._band, xunit = self._xunit, yunit = self._yunit, tolIR = self._tolIR, fracIntrinsicVar = self._fracIntrinsicVar, fracNoiseToSignal = self._fracNoiseToSignal, maxSigma = self._maxSigma, minTimescale = self._minTimescale, maxTimescale = self._maxTimescale)
+		lccopy = basicLC(self.numCadences, dt = self.dt, dtSmooth = self.dtSmooth, name = None, band = self.band, xunit = self.xunit, yunit = self.yunit, tolIR = self.tolIR, fracIntrinsicVar = self.fracIntrinsicVar, fracNoiseToSignal = self.fracNoiseToSignal, maxSigma = self.maxSigma, minTimescale = self.minTimescale, maxTimescale = self.maxTimescale)
 		lccopy.t = np.copy(self.t)
 		lccopy.x = np.copy(self.x)
 		lccopy.y = np.copy(self.y)
 		lccopy.yerr = np.copy(self.yerr)
 		lccopy.mask = np.copy(self.mask)
-		lccopy._p = self._p
-		lccopy._q = self._q
-		lccopy.XSim = np.copy(self.XSim)
-		lccopy.PSim = np.copy(self.PSim)
-		lccopy.XComp = np.copy(self.XComp)
-		lccopy.PComp = np.copy(self.PComp)
+		lccopy.pSim = self.pSim
+		lccopy.qSim = self.qSim
+		lccopy.pComp = self.pComp
+		lccopy.qComp = self.qComp
 		lccopy._mean = np.mean(lccopy.y)
 		lccopy._std = np.std(lccopy.y)
 		lccopy._mean = np.mean(lccopy.yerr)
@@ -1792,7 +1809,10 @@ class task(object):
 
 		for i in xrange(observedLC.numCadencesSmooth):
 			observedLC.xSmooth[i] = observedLC.XSmooth[i*observedLC.pComp]
-			observedLC.xerrSmooth[i] = math.sqrt(observedLC.PSmooth[i*observedLC.pComp*observedLC.pComp])
+			try:
+				observedLC.xerrSmooth[i] = math.sqrt(observedLC.PSmooth[i*observedLC.pComp*observedLC.pComp])
+			except ValueError:
+				observedLC.xerrSmooth[i] = 0.0
 		observedLC._isSmoothed = True
 
 		return res