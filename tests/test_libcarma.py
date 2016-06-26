ea3e847a02d9dfc551ebfa6c3ea87f511c4e2264
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2016-06-26 17:07:22 -0400

    added first test

---
+++ tests/test_libcarma.py
@@ -0,0 +1,14 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+try:
+	import libcarma as libcarma
+except ImportError:
+	print 'libcarma is not setup. Setup libcarma by sourcing bin/setup.sh'
+	sys.exit(1)
+
+def test_coeffs():
+	Rho = np.array([-1.0/10.0, 1.0])
+	Theta = libcarma.coeffs(1, 0, Rho)
+	assert -1.0*Theta[0] == Rho[0]
