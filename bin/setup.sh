6b003817bdb8e440103a78f575850e66592f5850
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2016-07-11 16:22:40 -0400

    Renamed package to kali

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,13 +1,13 @@
-# setup libcarma environment
+# setup kali environment
 #
 # source this file from your ~/.bashrc
 #
-# relative to <libcarma>/bin/
-LIBCARMA=$(cd "$(dirname "$BASH_SOURCE")/.."; pwd)
-LIBCARMA_CYTHON=$(cd "$(dirname "$BASH_SOURCE")/../python"; pwd)
-LIBCARMA_CYTHON_LIB=$(cd "$(dirname "$BASH_SOURCE")/../lib"; pwd)
+# relative to <kali>/bin/
+KALI=$(cd "$(dirname "$BASH_SOURCE")/.."; pwd)
+KALI_CYTHON=$(cd "$(dirname "$BASH_SOURCE")/../python"; pwd)
+KALI_CYTHON_LIB=$(cd "$(dirname "$BASH_SOURCE")/../lib"; pwd)
 
-export LIBCARMA
-export PYTHONPATH="$LIBCARMA_CYTHON:$PYTHONPATH"
-export PYTHONPATH="$LIBCARMA_CYTHON_LIB:$PYTHONPATH"
-echo "notice: libcarma tools have been set up."
+export KALI
+export PYTHONPATH="$KALI_CYTHON:$PYTHONPATH"
+export PYTHONPATH="$KALI_CYTHON_LIB:$PYTHONPATH"
+echo "notice: kali tools have been set up."
