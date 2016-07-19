2ffe88ae4e4fb028c4429cb54e174aad93667194
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2016-07-19 16:24:41 -0400

    Added STDLIB option on MacOSX

--- setup.py
+++ setup.py
@@ -29,16 +29,17 @@ System = platform.system()
 if System == 'Linux':
 	MKLLIBS = ['-L$MKLROOT/lib/intel64', '-lmkl_rt', '-lpthread', '-lm', '-ldl']
 elif System == 'Darwin':
+	VERFLAGS += ['-stdlib=libc++']
 	MKLLIBS = ['-L$MKLROOT/lib', '-Wl,-rpath,$MKLROOT/lib' , '-lmkl_rt', '-lpthread', '-lm', '-ldl']
 else:
 	MKLLIBS = []
 
 MKLDIR = MKLLIBS[0][2:-1]
 
-bSMBH_sourceList = ['bSMBH.pyx', 'binarySMBH.cpp', 'Constants.cpp']
+'''bSMBH_sourceList = ['bSMBH.pyx', 'binarySMBH.cpp', 'Constants.cpp']
 bSMBH_List = [os.path.join(os.environ['PWD'], 'src', srcFile) for srcFile in bSMBH_sourceList]
 
-bSMBH_ext = Extension(name='bSMBH', sources=bSMBH_List, language='c++', extra_compile_args = CPPFLAGS + VERFLAGS + ALIGHFLAGS + MKLFLAGS + OMPFLAGS, include_dirs=[INCLUDE, np.get_include()], extra_link_args = MKLLIBS + OMPLIBS + NLOPTLIBS, library_dirs = [MKLDIR], runtime_library_dirs = [MKLDIR])
+bSMBH_ext = Extension(name='bSMBH', sources=bSMBH_List, language='c++', extra_compile_args = CPPFLAGS + VERFLAGS + ALIGHFLAGS + MKLFLAGS + OMPFLAGS, include_dirs=[INCLUDE, np.get_include()], extra_link_args = MKLLIBS + OMPLIBS + NLOPTLIBS, library_dirs = [MKLDIR], runtime_library_dirs = [MKLDIR])'''
 
 rand_sourceList = ['rand.pyx', 'rdrand.cpp']
 rand_List = [os.path.join(os.environ['PWD'], 'src', srcFile) for srcFile in rand_sourceList]
