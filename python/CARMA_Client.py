e70dec233e6c09a347b480c13560419279f64b0a
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2016-08-25 11:26:57 -0400

    Added CARMA client & server

---
+++ python/CARMA_Client.py
@@ -0,0 +1,259 @@
+import zmq
+import warnings
+
+
+TIMEOUT = 1000 #milliseconds
+VERBOSE = False
+
+if __name__ == '__main__':
+	VERBOSE = True
+
+def printV(*args):
+
+	if VERBOSE:
+		for arg in args:
+			print arg,
+		print ''
+
+class SDSSError(Exception): #custom SDSSError that relates to serverside issues
+
+	def __init__(self, message, errors=None):
+
+		super(SDSSError, self).__init__(message)
+		self.errors = errors
+
+class ServerList(dict): #dictionary like class that manages the possible servers
+
+	def __init__(self, *args, **kwargs):
+
+		super(ServerList, self).__init__(*args, **kwargs)
+		self.best = None
+
+	def addServer(self, name, address, priority): # add a server to our list of servers
+		server = {"address":address, "priority":priority}
+		self[name] = server
+
+	def testServer(self, server):
+
+			try:
+				context = zmq.Context()
+				socket = context.socket(zmq.REQ)
+				socket.LINGER = False
+				socket.connect(server['address'])
+				socket.send(b"ping\n", flags = zmq.NOBLOCK)
+				if socket.poll(timeout = 1000, flags = zmq.POLLIN):
+					return True
+				else:
+					return False
+			except zmq.ZMQError as e:
+				raise SDSSError(e.message, e.errno)
+
+	def getBestServer(self): #determine the best server
+		for key, server in sorted(self.items(), key = lambda x: x[1]['priority']):
+			isGood = self.testServer(server)
+			if isGood:
+				printV("Best Server is %s" % key)
+				self.best = server['address']
+				break;
+		else:
+			self.best = None
+			raise SDSSError("No good servers available at the moment", self.best)
+
+	def setBestServer(self, server): #manually override the best server
+
+		printV("Testing Server %s" % server)
+		isGood = self.testServer(self[server])
+		if isGood:
+			self.best = self[server]['address']
+			printV("%s is now connected" % server)
+		else:
+			raise SDSSError("Bad Server: %s" % server, server)
+
+
+servers = ServerList()
+try:
+	servers.addServer("newton","tcp://newton.physics.drexel.edu:5001",2)
+except CARMA_Client.SDSSError as err:
+	warnings.warn(str(err))
+try:
+	servers.addServer("echidna","tcp://76.124.106.126:5001",1)
+except CARMA_Client.SDSSError as err:
+	warnings.warn(str(err))
+try:
+	servers.addServer("vish15","tcp://vish15.physics.upenn.edu:5001",0)
+except CARMA_Client.SDSSError as err:
+	warnings.warn(str(err))
+servers.getBestServer()
+
+#Temporary while vish15 is out of date:
+#servers.setBestServer("echidna")
+###
+
+def getSocket():
+
+	context = zmq.Context()
+	socket = context.socket(zmq.REQ)
+	socket.LINGER = False
+	socket.connect(servers.best)
+	return socket
+
+def zmqSocketDecorator(func): #a decorator that handles the zmq sockets and raises SDSS exceptions
+
+	def wrapper(*args, **kwargs):
+		try:
+			socket = getSocket()
+			return func(socket, *args, **kwargs)
+		except zmq.ZMQError as e:
+			raise SDSSError(e.message, e.errno)
+
+	return wrapper
+
+@zmqSocketDecorator
+def getCommandResult(socket, cmd): #send a command to the server and return the result
+
+	socket.send(cmd)
+	if socket.poll(timeout=TIMEOUT, flags = zmq.POLLIN):
+		result = socket.recv_pyobj(flags = zmq.NOBLOCK)
+	else:
+		raise SDSSError("Socket timed out",TIMEOUT)
+	if isinstance(result, Exception):
+		raise SDSSError(*result.args)
+
+	return result
+
+def createCommand(server_func, *args): #get the command string for a function and it's arguments
+
+	if len(args):
+		args = " ".join(map(str, args))
+	else:
+		args = ''
+	cmd = b"%s\n%s" % (server_func, args)
+	return cmd
+
+def isValid(server_func): #checks if a server_func is valid
+
+	cmd = createCommand('isValid', server_func)
+	result = getCommandResult(cmd)
+	return result
+
+def commandArgCount(server_func): #gets information about the server func
+
+	cmd = createCommand('argCount', server_func)
+	result = getCommandResult(cmd)
+	return result
+
+def createFunction(server_func, docstr = None):
+	#Create a function object that acts on a server side func with name 'server_func'
+	if isValid(server_func):
+		nargs = commandArgCount(server_func) - 1
+	else:
+		raise SDSSError("Invalid Function: %s" % server_func, server_func)
+	def Func(*args):
+		if len(args) != nargs:
+			message = "%s takes exactly %i arguments (%i given)" % (server_func, nargs, len(args))
+			raise TypeError(message)
+		if docstr is not None:
+			Func.__doc__ = docstr
+		cmd = createCommand(server_func, *args)
+		result = getCommandResult(cmd)
+		return result
+	return Func
+
+
+#define our client-side functions below
+getRandLC = createFunction("randLC",
+"""
+args: None
+returns:
+	filename, redshift, data (tuple):
+		filename (str): name of the file on disk
+		redshift (float): redshift of the object
+		data (numpy structure array): structured array of the data from the LC file
+""")
+
+getLC = createFunction("getLC",
+"""
+args:
+	ID (str): SDSS J2000 name
+returns:
+	filename, redshift, data (tuple):
+		filename (str): name of the file on disk
+		redshift (float): redshift of the object
+		data (numpy structure array): structured array of the data from the LC file
+""")
+
+getIDList = createFunction("IDList",
+"""
+args: None
+returns:
+	IDList (list): List of strings of SDSS Objects names on disk
+""")
+
+getNearestLC = createFunction('getNearestLC',
+"""
+args:
+	ID (str): SDSS J200 name
+	tol (float): matching tolerance in degrees
+returns:
+	filename, reshift, data (tuple):
+		see above
+""")
+
+if __name__ == '__main__':
+	import sys
+	if len(sys.argv) == 1:
+		print "Test"
+	if sys.argv[1] == '--check':
+		for name in sys.argv[2:]:
+			try:
+				getNearestLC(name, 2/60.0/60.0)
+			except SDSSError as e:
+				if 'No objects in list' in e.message:
+					print "LC does not exist in data base", 0, name
+			except IndexError as e:
+				print "No File Specified"
+			else:
+				print "LC does exist in database     ", 1, name
+	elif sys.argv[1] == '--rand':
+		print getRandLC()
+	elif sys.argv[1] == '--list':
+		print getIDList()
+	elif sys.argv[1] == '--all':
+		good = 0
+		print "Trying to get a random LC"
+		try:
+			getRandLC()
+			print "Good"
+			good += 1
+		except Exception as e:
+			print e
+			print "Bad"
+		print "Trying to get the list of ID numbers"
+		try:
+			getIDList()
+			print "Good"
+			good += 1
+		except Exception as e:
+			print e
+			print "Bad"
+		print "Trying to get a specific LC"
+		try:
+			getLC("013013.62+000253.5")
+			print "Good"
+			good += 1
+		except Exception as e:
+			print e
+			print "Bad"
+		print "Trying to catch an invalid LC"
+		try:
+			getLC("013013.62+000253.6")
+		except SDSSError as e:
+			print "Good"
+			good += 1
+		except Exception as e:
+			print e
+			print "Bad"
+		if good == 4:
+			print "All tests successful"
+		else:
+			print "Some tests failed, Errors were found"
---
+++ python/CARMA_Server.py
@@ -0,0 +1,391 @@
+import zmq, sys, os, time, cPickle, StringIO, numpy as np, re, random
+import carmcmc as cm
+import multiprocessing as mp
+from JacksTools import jio
+
+
+class Server:
+
+	__version__ = "0.0.1"
+
+	def __init__(self):
+
+		self.context = zmq.Context(io_threads = 4)
+
+		self.socket = self.context.socket(zmq.REP)
+		self.socket.bind('tcp://*:5001')
+		self.running = True
+		self.randBytes = None
+
+		self.commands = {
+		'getLC':self.getLC, 
+		'randLC':self.randLC,
+		'IDList':self.getIDList,
+		'isValid':self.isValidCommand,
+		'argCount':self.getArgCount,
+		'ping': self.respond,
+		'getRawLC':self.getRawLC,
+		'getAllRawLC':self.getAllRawLC,
+		'getNearestLC':self.getNearestLC,
+		'getFileInfo':self.getFileInfo,
+		'getAllFileInfo':self.getAllFileInfo}
+	
+		print "Server Started on %s" % FileManager.root
+
+	#Below here are functions that can be called by the user
+
+	def respond(self): #respond to arbitrary request
+
+		self.socket.send_pyobj(1)
+		print "Server has been pinged"
+
+	def getArgCount(self, command):
+
+		result = self.commands[command].func_code.co_argcount
+		self.socket.send_pyobj(result)
+		print "Sent arg count for %s" % command
+
+	def isValidCommand(self, server_func):
+
+		isValid = False
+		if server_func in self.commands:
+			isValid = True
+		self.socket.send_pyobj(isValid)
+		print "Sent %s" % str(isValid)
+
+	def getLC(self, ID):
+
+		fname = os.path.join(FileManager.LCDir, FileManager.getFileName(ID))
+		data = jio.load(fname, delimiter = ',', headed = True)
+		z = FileManager.getRedshift(ID)
+		package = (fname, z, data)	
+		self.socket.send_pyobj(package)
+		print "Sent %s" % FileManager.getFileName(ID)
+
+	def getNearestLC(self, ID, tol):
+
+		tol = float(tol)
+		ra = float(ScienceManager.hmsToDeg(ID[:9]))
+		dec = float(ScienceManager.dmsToDeg(ID[9:]))
+		coords = FileManager.getCoordList()		
+		coords = zip(range(len(coords)), coords)
+		nearest = None
+		sep = 360
+		for i, (Ra, Dec) in coords:
+			dist = np.arccos(np.sin(dec)*np.sin(Dec)+np.cos(dec)*np.cos(Dec)*np.cos(ra - Ra))
+			if dist < tol:
+				if (dist < sep):
+					sep = dist
+					nearest = i
+		if nearest is not None:
+			ID = FileManager.IDList(FileManager.getLCList())[nearest]
+			self.getLC(ID)		
+		else:
+			raise IndexError("No objects in list within tolerance")
+
+	def getIDList(self):
+
+		idlist = FileManager.IDList(FileManager.getLCList())
+		self.socket.send_pyobj(idlist)
+		print "Sent %s" % "id list"
+
+	def randLC(self):
+
+		ID = random.choice(FileManager.IDList(FileManager.getLCList()))
+		self.getLC(ID)
+
+	def getRawLC(self, ID):
+
+		fname = os.path.join(FileManager.LCDir, FileManager.getFileName(ID))
+		with open(fname,'rb') as f:
+			raw = f.read()
+		self.socket.send_pyobj(raw)
+		print "Sent raw data for %s" % ID
+
+	def getAllRawLC(self, *IDList): #get all the data for all IDs in the list
+
+		DataList = []
+		for ID in IDList:
+			fname = os.path.join(FileManager.LCDir, FileManager.getFileName(ID))
+			with open(fname,'rb') as f:
+				DataList.append(f.read())
+		self.socket.send_pyobj(DataList)
+		print "Sent all the data on disk"
+
+	def getFileInfo(self, filename):
+
+		package = FileManager.getFileInfo(filename)
+		self.socket.send_pyobj(package)
+		print "Sent file information for %s" % ID
+
+	def getAllFileInfo(self):
+
+		
+		package = map(FileManager.getFileInfo, FileManager.getLCList())
+		self.socket.send_pyobj(package)
+		print "Sent information on all files"
+
+	#below here are server specific (sort of private) functions
+
+	def sync(self, server): #sync the LCDir of the server to another server
+		'''steps for the sync:
+		1. Get List of Files on remote server
+		2. Compare file to local files
+		2.a. Get files that do not exist on this server
+		2.b. Get files that have been updated less recently
+		3. sync files
+		4. update last updated time
+		'''
+		try:
+			context = zmq.Context()
+			socket = context.socket(zmq.REQ)
+			socket.LINGER = False
+			socket.connect(server)
+			
+		except zmq.ZMQError as e:
+			print e
+		else:
+			try:
+				socket.send(b"IDList\n")
+				if socket.poll(timeout = 2000, flags = zmq.POLLIN):
+					idList = socket.recv_pyobj()
+					print "Got List of ID numbers"
+				else:
+					idList = []
+					print "Sync Failed, could not get ID List"
+				socket.send(b"getAllFileInfo\n")
+				if socket.poll(timeout = 10000, flags = zmq.POLLIN):
+					stats = socket.recv_pyobj()
+					print "Get File Information"
+				else:
+					print "Sync Failed, could not get Stats List"
+			except zmq.ZMQError as e:
+				print e
+			else:
+				localIDs = FileManager.IDList(FileManager.getLCList())
+				localStats = dict(zip(localIDs,map(FileManager.getFileInfo, FileManager.getLCList())))
+				stats = dict(zip(idList, stats))
+				matchIDs = list((set(localIDs) & set(idList)))
+				new = list(set(idList) - set(localIDs))
+				updates = [ID for ID in matchIDs if stats[ID].st_ctime > localStats[ID].st_ctime]			
+				idList = list(set(new)|set(updates))
+				print "About to sync %i files" % len(idList)
+				print "Please wait, this may take up to 20 minutes" 
+				IDList = " ".join(idList)
+				try:
+					for i, ID in enumerate(idList):
+						socket.send(b"getRawLC\n%s" % ID)
+						if socket.poll(timeout = 5000, flags = zmq.POLLIN):
+							print "Got %s" % ID, "%i / %i" % (i, len(idList))
+							result = socket.recv_pyobj()
+							FileManager.updateLC([ID], [result])
+						else:
+							print "Timeout, Failed to get %s" & ID
+					print "Sync Complete"
+				except zmq.ZMQError as e:
+					print e
+					print "Sync Failed, Server Error"
+
+	def ErrorMsg(self, *args):
+		
+		error = Exception(*args)
+		self.socket.send_pyobj(error)
+		print "Sent Exception %s" % args[0]
+
+	def isRunning(self):
+
+		return self.running
+
+	def stop(self):
+
+		self.running = False
+
+	def quit(self):
+
+		self.running = False
+		self.socket.close()
+		sys.exit(0)
+
+	def processCMD(self, cmd): # process a command input from keyboard
+
+		if cmd.lower() in ['exit','quit']:
+			print "Quitting"
+			self.quit()
+		elif cmd.lower() in ['stop']:
+			if self.isRunning():
+				print "Stopping Server"
+				self.stop()
+				return True
+			else:
+				print "The server is already stopped"
+				return True
+		elif cmd.lower() in ['start']:
+			if self.isRunning():
+				print "The server is already running"
+				return True
+			else:
+				print "Starting Server"
+				self.start()
+				return False
+		elif cmd.lower() in ['sync']:
+			server = raw_input("Server: ")
+			self.sync(server)
+	
+
+	def start(self):
+
+		self.running = True
+		while self.isRunning():
+			try:
+				message = self.socket.recv().split('\n')
+				command = message[0]
+				args = message[1].split()
+				print "GOT:",command
+				print "   ARGS:", args
+				self.commands[command](*args)
+
+			except KeyboardInterrupt as k:
+				print "KeyboadInterrupt detected, would you like to do something?"
+				cmd = raw_input("=> ")
+				self.processCMD(cmd)
+				
+			except Exception as e:
+				print "Failed"
+				print e
+				self.ErrorMsg(*e.args)
+
+		cmd = raw_input("=> ")
+		while self.processCMD(cmd):
+			cmd = raw_input("=> ")
+
+class FileManager:
+
+	if "S82DATADIR" in os.environ:
+		root = os.environ['S82DATADIR']
+	else:
+		print "S82DATADIR not found"
+		root = ''
+		while not os.path.isdir(root):
+			root = raw_input("Root Dir: ")
+
+	Pattern = r"LC_(.*)_Calibrated\.csv"	
+	LCDir = os.path.join(root,"Data/LC/Calibrated")
+	PickleDir = os.path.join(root,"/Pickles")
+	RedshiftFile = os.path.join(root,"Data/Stripe82ObjectList.dat")
+	zList = []
+	regex = re.compile(Pattern)
+	with open(RedshiftFile,'rb') as f:
+		zList = [line.split() for line in f]
+
+	def __init__(self):
+		pass
+	
+	@classmethod
+	def getzList(self):
+
+		return self.zList
+
+	@classmethod
+	def getLCList(self):
+
+		return os.listdir(self.LCDir)
+
+	@classmethod
+	def getPickleList(self):
+
+		return os.listdir(self.PickleDir)
+
+	@classmethod
+	def IDList(self, List):
+
+		return self.regex.findall('\n'.join(List))
+
+	@classmethod
+	def getProcessed(self):
+
+		return self.IDList(self.getPickleList())
+
+	@classmethod
+	def getUnprocessed(self):
+
+		return list(set(self.IDList(self.getLCList())) - set(self.getProcessed()))
+
+	@classmethod
+	def getFileName(self, ID):
+
+		return self.getLCList()[self.IDList(self.getLCList()).index(ID)]
+
+	@classmethod
+	def getFileInfo(self, filename):
+		
+		return os.stat(os.path.join(self.LCDir,filename))
+
+	@classmethod
+	def getRedshift(self, ID):
+
+		index = [i[0] for i in self.zList].index(ID) #1 is the index for the ID number
+		return self.zList[index][3] #3 is the index for redshift
+
+	@classmethod
+	def getCoordList(self):
+
+		return map(lambda x: (float(ScienceManager.hmsToDeg(x[:9])), float(ScienceManager.dmsToDeg(x[9:]))), self.IDList(self.getLCList()))
+
+	@classmethod
+	def updateLC(self, IDList, dataList):
+	
+		for ID, data in zip(IDList, dataList):
+			filename = os.path.join(self.LCDir, ID.join(("LC_","_Calibrated.csv")))	
+			with open(filename,'w') as f:
+				f.write(data)
+
+	#below here manage the server files
+
+class ScienceManager:
+
+	@classmethod
+	def degTohms(self, deg):
+	
+		deg = float(deg)	
+		h = deg/360.0*24
+		m = (h - int(h))*60
+		s = (m - int(m))*60
+		h = str(int(h)).zfill(2)
+		m = str(int(m)).zfill(2)
+		s = '.'.join((str(int(s)).zfill(2), str(int((s - int(s))*100)).zfill(2)))
+		return ''.join((h,m,s))
+
+	@classmethod
+	def degTodms(self, deg):
+		
+		deg = float(deg)
+		d = dec*(-1 if deg < 1 else 1)
+		m = (d - int(d))*60
+		s = (m - int(m))*60
+		d = str(int(d)).zfill(2)
+		m = str(int(m)).zfill(2)
+		s = '.'.join((str(int(s)).zfill(2), str(int((s - int(s))*100))[0]))
+		return ''.join((d,m,s))
+
+	@classmethod			
+	def dmsToDeg(self, dms):
+
+		d, m, s = map(float,(dms[:3], dms[3:5], dms[5:]))
+		if d > 0:
+			deg = d + m/60.0 + s/60.0/60.0
+		else:
+			deg = d - m/60.0 - s/60.0/60.0
+		return str(deg)
+
+	@classmethod
+	def hmsToDeg(self, hms):
+		h,m,s = map(float, (hms[:2], hms[2:4], hms[4:]))
+		deg = h/24.0*360.0 + m/24.0*360.0/60.0 + s/24.0*360.0/60.0/60.0
+		return str(deg)
+
+
+if __name__ == '__main__':
+
+	S = Server()
+	S.start()
+	
